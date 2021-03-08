90bd691583b59d63cfabd6382b4ee9491419939d
Guillermo Esquivel <73927126+Demodes1@users.noreply.github.com>
2021-03-08 11:10:57 -0600

    Update frecuencia.py

--- frecuencia.py
+++ frecuencia.py
@@ -1,11 +1,11 @@
 import pandas as pd 
-import csv
 data = pd.read_csv('./data.csv')
 data.head()
 # El dato total te da la frecuencia
 total = (data 
-  .groupby("¿Usas cubrebocas en los lugares públicos?")
-  .agg(frequency=("¿Usas cubrebocas en los lugares públicos?", "count")))
+  .groupby("¿Usas cubrebocas en los lugares públicos?") # Aqui se tiene que escribir el nombre de la columna
+  .agg(frequency=("¿Usas cubrebocas en los lugares públicos?", "count"))) # Y aqui tambien
 # Estas funciones en base a la frecuencia, te dan la frecuencia acomulada
 total["cum_frequency"] = total["frequency"].cumsum()
-print(total)
+print(total) # Frecuenca
+print(total/99) # Frecuencia relativa (99 es total de elementos)
